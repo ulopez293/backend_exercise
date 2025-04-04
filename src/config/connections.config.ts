1049fae1028c2cd1f34f73aed494e789da3d5990
Ubaldo Lopez <ulopez293@gmail.com>
2025-04-03 21:23:31 -0600

    config

--- src/config/connections.config.ts
+++ src/config/connections.config.ts
@@ -1,20 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-const railwayEnv = process.env.RAILWAY_ENV || 'development'
+// const railwayEnv = process.env.RAILWAY_ENV || 'development'
 
-console.log(`Environment Railway: ${railwayEnv}`)
+// console.log(`Environment Railway: ${railwayEnv}`)
 
-export const conectionMongoDB = (railwayEnv === 'production') ? {
+export const conectionMongoDB = {
     MongoDB: {
         api: {
-            uri: process.env.MONGO_DB_URI_PROD ?? ``,
-        }
-    }
-} : {
-    MongoDB: {
-        api: {
-            uri: process.env.MONGO_DB_URI_DEV ?? ``
+            uri: process.env.MONGO_DB_URI ?? ``,
         }
     }
 }
