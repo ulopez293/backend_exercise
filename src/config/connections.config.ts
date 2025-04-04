ee55cbda7a331128d79b53976440f2f79aa84d34
Ubaldo Lopez <ulopez293@gmail.com>
2025-04-03 22:20:45 -0600

    registro

--- src/config/connections.config.ts
+++ src/config/connections.config.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
-
 // const railwayEnv = process.env.RAILWAY_ENV || 'development'
-
 // console.log(`Environment Railway: ${railwayEnv}`)
+
+console.log("process.env.MONGO_DB_URI: ", process.env.MONGO_DB_URI)
 
 export const conectionMongoDB = {
     MongoDB: {
--- src/controllers/registerUser.controller.ts
+++ src/controllers/registerUser.controller.ts
@@ -7,33 +7,34 @@ const registerSchema = z.object({
     name: z.string().trim().min(1, "El nombre es obligatorio"),
     email: z.string().trim().email("El correo no es válido"),
     password: z.string().trim().min(6, "La contraseña debe tener al menos 6 caracteres"),
-})
+}).strict()
 
 export const registerUserController = async (req: Request, res: Response) => {
     try {
         const parsedData = registerSchema.parse(req.body)
         const { name, email, password } = parsedData
 
-        if (!name || !email || !password) {
-            return res.status(400).json({ error: "Todos los campos son obligatorios" })
-        }
-
         const existingUser = await UsuariosModel.findOne({ email })
-        if (existingUser) {
-            return res.status(400).json({ error: "El correo ya está registrado" })
-        }
+        if (existingUser) return res.status(400).json({ error: "El correo ya está registrado" })
+
 
         const hashedPassword = await bcrypt.hash(password, 10)
-
         const newUser = await UsuariosModel.create({
             name,
             email,
             password: hashedPassword
         })
 
-        res.status(201).json({ message: "Usuario registrado exitosamente", user: newUser })
+        res.status(201).json({ 
+            message: "Usuario registrado exitosamente", 
+            user: {
+                name: newUser.name,
+                email: newUser.email,
+                id: newUser._id
+            } 
+        })
     } catch (err) {
         console.error(err)
-        res.status(500).json({ error: "Error al registrar usuario" })
+        res.status(500).json({ error: "Error al registrar usuario", err: err })
     }
 }
